a40c5590178bf19b91053507ad4db156c302ad23
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-01-21 12:10:45 -0300

    Add project files.

---
+++ CrudDapper/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace CrudDapper.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Position { get; set; }
+
+        public double Salary { get; set; }
+        public string CPF { get; set; }
+        public  double Situation { get; set; } // 1- Active ; 0 - Inactive
+        public string Password { get; set; }
+    }
+}
