09f3af42453b46483ff1569c6265fcf460bb01b5
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-01-22 12:39:32 -0300

    feat: defining connection string feat: creating first DTO to return a user list on GET method

--- CrudDapper/Services/UserService.cs
+++ CrudDapper/Services/UserService.cs
@@ -1,6 +1,18 @@
-﻿namespace CrudDapper.Services
+﻿using CrudDapper.Dto;
+using CrudDapper.Models;
+
+namespace CrudDapper.Services
 {
     public class UserService : IUserInterface
     {
+        private readonly IConfiguration _configuration;
+        public UserService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public Task<ResponseModel<List<UserListDto>>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
